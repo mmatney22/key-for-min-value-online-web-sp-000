297c53b09a7fe899a044c84f9495321c9e80af18
Morgan Matney <morgan.matney89@gmail.com>
2020-07-18 22:49:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
   name_hash.each do |key, value|
-    if
+    if lowest_value == nil || value < lowest_value
       key = lowest_key
   end
-  name
+  lowest_key
 end
